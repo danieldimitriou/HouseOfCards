4db4588cc599050ef649eede2e6ac10cd7013c0a
stefanbordea <apprentice0@protonmail.com>
2021-05-20 10:39:30 +0300

    Fix label spacing on the end stage and change font size and weight

--- src/sample/EndStageController.java
+++ src/sample/EndStageController.java
@@ -17,7 +17,6 @@ import java.util.ResourceBundle;
 public class EndStageController implements Initializable {
 
     @FXML private Label resultLabel;
-    @FXML private Label result;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
